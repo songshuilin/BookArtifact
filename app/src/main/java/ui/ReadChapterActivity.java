a316adb733b6a0b0b98450a3fd8b2ff1e18161a5
songshuilin <631659521@qq.com>
2016-11-21 17:00:33 +0800

    完成有声小说

--- app/src/main/java/ui/ReadChapterActivity.java
+++ app/src/main/java/ui/ReadChapterActivity.java
@@ -1,18 +1,35 @@
 package ui;
 
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.edu.bookartifact.R;
+import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechSynthesizer;
+import com.iflytek.cloud.SynthesizerListener;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 import adapter.NovelFragmentAdapter;
 import bean.NovelChapterContent;
@@ -35,14 +52,28 @@ public class ReadChapterActivity extends AppCompatActivity {
     private NovelFragmentAdapter adapter;
     private ArrayList<String> list;
     private int curChapter;//当前的章节
+    private String content;
+    private boolean isChanged = false;
+    private String[] sizeName = {"小", "中", "大"};
+    private int whereOne;
+    private String name;
+    private boolean isNight = false;
+    private boolean isVoice = false;
+    private String[] readPeopleName = new String[]{
+            "普通话(男)", "普通话(女)"
+            , "汉语(东北话)", "汉语(河南话)", "中英文(粤语)"
+            , "汉语(四川话)", "汉语(陕西话)"
+    };
+    private SpeechSynthesizer mTts;//创建 SpeechSynthesizer 对象
+    private int curPagePosition;
     Handler handler = new Handler() {
         @Override
-        public void handleMessage(Message msg) {
+        public synchronized void handleMessage(Message msg) {
             super.handleMessage(msg);
             switch (msg.what) {
                 case 0x123:
                     Log.i("TAG", "handleMessage: " + chapterContent);
-                    String content = chapterContent.getNovelChapterContent();
+                    content = chapterContent.getNovelChapterContent();
                     int count = content.length() % 300;
                     totalPage = content.length() / 300;
                     if (count != 0) {
@@ -81,9 +112,9 @@ public class ReadChapterActivity extends AppCompatActivity {
                     break;
                 case 0x1234:
                     curChapter++;
-                    String nextContent = chapterContent.getNovelChapterContent();
-                    int nextCount = nextContent.length() % 300;
-                    totalPage = nextContent.length() / 300;
+                    content = chapterContent.getNovelChapterContent();
+                    int nextCount = content.length() % 300;
+                    totalPage = content.length() / 300;
                     if (nextCount != 0) {
                         totalPage++;
                     }
@@ -95,10 +126,10 @@ public class ReadChapterActivity extends AppCompatActivity {
                     for (int i = 0; i < totalPage; i++) {
                         nextStart = i * 300;
                         nextEnd = (i + 1) * 300;
-                        if (nextEnd > nextContent.length()) {
-                            nextEnd = nextContent.length();
+                        if (nextEnd > content.length()) {
+                            nextEnd = content.length();
                         }
-                        String subChapterContent = nextContent.substring(nextStart, nextEnd);
+                        String subChapterContent = content.substring(nextStart, nextEnd);
                         NovelChapterFragment fragment = new NovelChapterFragment();
                         fragments.add(fragment);
                         //传递数据给fragment
@@ -115,7 +146,6 @@ public class ReadChapterActivity extends AppCompatActivity {
                     }
                     adapter.notifyDataSetChanged();
                     allTotalPage += totalPage;
-
                     break;
             }
 
@@ -127,14 +157,16 @@ public class ReadChapterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_chapter);
         initViews();
+        initSpeechSynthesizerPar();
 //        NovelChapter chapter= (NovelChapter) getIntent().getSerializableExtra("chapter");
 //        String path=chapter.getChapterPath();
         path = getIntent().getStringExtra("path");
         list = getIntent().getStringArrayListExtra("chaptersUrl");
-        curChapter=list.indexOf(path);
+        curChapter = list.indexOf(path);
 //        Log.i("TAGss", "onCreate: "+list.toString());
         getChapterContent(path);//异步获取内容
     }
+
 
     /**
      * 异步获取内容
@@ -173,7 +205,9 @@ public class ReadChapterActivity extends AppCompatActivity {
     }
 
     private void initViews() {
+
         mViewPage = (ViewPager) findViewById(R.id.chapter_viewpage);
+//        mViewPage.setOffscreenPageLimit(0);
         mViewPage.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -182,20 +216,215 @@ public class ReadChapterActivity extends AppCompatActivity {
 
             @Override
             public void onPageSelected(int position) {
-                Log.i("TAG", "onPageSelected:..... position"+position);
+                curPagePosition = position;
                 if (position == allTotalPage - 1) {
-                    if (curChapter < list.size()-1) {
+                    if (curChapter < list.size() - 1) {
                         getNextChapterContent(list.get(curChapter + 1));
-                        Log.i("TAG", "onPageSelected: " + (allTotalPage - 1));
                     }
                 }
+
+                if (isVoice) {
+                    TextView tv = (TextView) fragments.get(position).getView().findViewById(R.id.chapter_content);
+                    name = getPeopleName(readPeopleName[whereOne]);
+                    mTts.setParameter(SpeechConstant.VOICE_NAME, name);
+                    //开始合成
+                    mTts.startSpeaking(tv.getText().toString(), listener);
+                }
+
             }
 
             @Override
             public void onPageScrollStateChanged(int state) {
-
+                if (isChanged) {
+                    if (name != null) {
+                        EventBus.getDefault().post(getSize(name));
+                    }
+                }
+                EventBus.getDefault().post(isNight);
             }
         });
     }
 
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        menu.setHeaderTitle("设置操作");
+        menu.setHeaderIcon(R.drawable.logo);
+        menu.add(0, 1, 0, "字体");
+        menu.add(0, 2, 0, "夜间模式");
+        menu.add(0, 3, 0, "有声小说");
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 1:
+                Log.i("TAG", "上下文菜单一: ");
+                AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+                dialog.setTitle("设置字体大小");
+                dialog.setIcon(R.drawable.logo);
+                dialog.setCancelable(false);
+                dialog.setSingleChoiceItems(sizeName, 1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        whereOne = which;
+                        Log.i("TAGaa", "setSingleChoiceItems: " + sizeName[which]);
+                    }
+                });
+                dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        isChanged = true;
+                        if (isChanged) {
+                            name = sizeName[whereOne];
+                            int size = getSize(name);
+                            Log.i("TAGaa", "onClick:............. " + size);
+                            EventBus.getDefault().post(size);
+                        }
+                    }
+                });
+                dialog.setNegativeButton("取消", null);
+                dialog.show();
+                break;
+            case 2:
+                Log.i("TAG", "上下文菜单二: ");
+                isNight = !isNight;
+                EventBus.getDefault().post(isNight);
+                break;
+            case 3:
+                Log.i("TAG", "上下文菜单三: ");
+                AlertDialog.Builder dialog1 = new AlertDialog.Builder(this);
+                dialog1.setTitle("设置发音人");
+                dialog1.setIcon(R.drawable.logo);
+                dialog1.setCancelable(false);
+                dialog1.setSingleChoiceItems(readPeopleName, 1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        whereOne = which;
+                    }
+                });
+                dialog1.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        isVoice = true;
+                    }
+                });
+                dialog1.setNegativeButton("取消", null);
+                dialog1.show();
+                break;
+        }
+        return true;
+    }
+
+
+    private int getSize(String sizeName) {
+        switch (sizeName) {
+            case "小":
+                return 14;
+
+            case "中":
+                return 16;
+
+            case "大":
+                return 20;
+        }
+        return 0;
+    }
+
+
+    private String getPeopleName(String peopleName) {
+        switch (peopleName) {
+            case "普通话(女)":
+                return "xiaoyan";
+
+            case "普通话(男)":
+                return "xiaoyu";
+
+            case "汉语(东北话)":
+                return "vixyun";
+
+            case "汉语(河南话)":
+                return "vixk";
+
+            case "中英文(粤语)":
+                return "vixm";
+
+            case "汉语(四川话)":
+                return "vixr";
+
+            case "汉语(陕西话)":
+                return "vixying";
+        }
+
+        return null;
+    }
+
+    /**
+     * 设置语音合成的参数
+     */
+    private void initSpeechSynthesizerPar() {
+        //1.创建 SpeechSynthesizer 对象, 第二个参数：本地合成时传 InitListener
+        mTts = SpeechSynthesizer.createSynthesizer(this, new InitListener() {
+            @Override
+            public void onInit(int i) {
+
+            }
+        });
+        // mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_LOCAL);
+        //2.合成参数设置
+        mTts.setParameter(SpeechConstant.SPEED, "50");//设置语速
+        mTts.setParameter(SpeechConstant.VOLUME, "80");//设置音量，范围 0~100
+        mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD); //设置云端
+        mTts.setParameter(SpeechConstant.TTS_AUDIO_PATH, "./sdcard/iflytek.pcm");
+    }
+
+    private SynthesizerListener listener = new SynthesizerListener() {
+
+        @Override
+        public void onSpeakResumed() {
+            Log.i("TAG", "onSpeakResumed: ");
+        }
+
+        @Override
+        public void onSpeakProgress(int arg0, int arg1, int arg2) {
+            Log.i("TAG", "onSpeakProgress: ");
+
+        }
+
+        @Override
+        public void onSpeakPaused() {
+            Log.i("TAG", "onSpeakPaused: ");
+
+        }
+
+        @Override
+        public void onSpeakBegin() {
+            Log.i("TAG", "onSpeakBegin: ");
+
+        }
+
+        @Override
+        public void onEvent(int arg0, int arg1, int arg2, Bundle arg3) {
+            Log.i("TAG", "onEvent: ");
+
+        }
+
+        @Override
+        public void onCompleted(SpeechError arg0) {
+            mViewPage.setCurrentItem(curPagePosition + 1);
+        }
+
+        @Override
+        public void onBufferProgress(int arg0, int arg1, int arg2, String arg3) {
+            Log.i("TAG", "onBufferProgress: ");
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        mTts.stopSpeaking();//停止合成
+        super.onDestroy();
+    }
 }
